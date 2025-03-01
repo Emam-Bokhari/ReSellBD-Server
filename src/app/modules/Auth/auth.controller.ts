30e450d9913e008874b5ae875f5c1adf7e4ac6d0
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-01 11:36:50 +0600

    feat(auth): add user registration controller with async handling

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { asyncHandler } from "../../../utils/global/asyncHandler";
+import { sendResponse } from "../../../utils/global/sendResponse";
+import { AuthServices } from "./auth.service";
+
+const registerUserController = asyncHandler(async (req, res) => {
+    const registerUserPayload = req.body;
+    const registeredUser = await AuthServices.registerUser(registerUserPayload);
+
+    sendResponse(res, {
+        success: true,
+        message: 'User registered successfully',
+        statusCode: 201,
+        data: registeredUser,
+    });
+});
+
+export const AuthControllers = {
+    registerUserController,
+}
