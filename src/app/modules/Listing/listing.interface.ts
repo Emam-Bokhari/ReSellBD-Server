7e33d8d7cc0c183fe920b187775fbf0d1b253dd4
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-02 05:55:55 +0600

    feat(listing): add category, status, and additional fields to TListing type

---
+++ src/app/modules/Listing/listing.interface.ts
@@ -0,0 +1,17 @@
+import { Types } from "mongoose";
+
+export type TListing = {
+    title: string;
+    description: string;
+    price: number;
+    condition: "new" | "likeNew" | "used" | "refurbished";
+    images: string[];
+    userID: Types.ObjectId;
+    status: "available" | "sold";
+    category: "mobiles" | "electronics" | "vehicles" | "property" | "home" | "pets" | "cloths" | "sports";
+    brand?: string;
+    location: string;
+    negotiable?: boolean;
+    warranty?: "yes" | "no";
+    contactNumber?: string;
+}
