9a34149e21785f026d18844ac04d6b710cc81507
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-02 11:19:45 +0600

    types: add TTransaction type for transaction entities

---
+++ src/app/modules/Transaction/transactions.interface.ts
@@ -0,0 +1,8 @@
+import { Types } from "mongoose"
+
+export type TTransaction = {
+    buyerID: Types.ObjectId;
+    sellerID: Types.ObjectId;
+    itemID: Types.ObjectId;
+    status: "pending" | "completed";
+}
