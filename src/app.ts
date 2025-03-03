4693e2b5cd6a82475a62477f8fe56a231c777774
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-03 11:29:53 +0600

    feat: add cookie parser and vercel.json file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
 import { globalErrorHandler } from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
-const app = express();
+import cookieParser from 'cookie-parser';
+
+const app: Application = express();
 
 // parser
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cookieParser());
+
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 
 // application routes
 app.use('/api/v1', router);
