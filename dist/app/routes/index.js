71870f92d17eae6716e71068314ee164495fc7bf
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-03 11:33:27 +0600

    chore: build project

---
+++ dist/app/routes/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const auth_route_1 = require("../modules/Auth/auth.route");
+const user_route_1 = require("../modules/User/user.route");
+const listing_route_1 = require("../modules/Listing/listing.route");
+const transaction_route_1 = require("../modules/Transaction/transaction.route");
+const router = express_1.default.Router();
+const moduleRoutes = [
+    {
+        path: '/auth',
+        route: auth_route_1.AuthRoutes,
+    },
+    {
+        path: '/users',
+        route: user_route_1.UserRoutes,
+    },
+    {
+        path: '/listings',
+        route: listing_route_1.ListingRoutes,
+    },
+    {
+        path: '/transactions',
+        route: transaction_route_1.TransactionRoutes,
+    },
+];
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+exports.default = router;
