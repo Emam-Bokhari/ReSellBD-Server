797466845d25060ee915dc6eabaa3b2b9d3cf911
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-23 11:29:57 +0600

    feat(newsLetter): add NewsLetter schema and model

---
+++ src/app/modules/NewsLetter/newsLetter.model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+import { TNewsLetter } from "./newsLetter.interface";
+
+const newsLetterSchema = new Schema<TNewsLetter>({
+    email: {
+        type: String,
+        required: true,
+    }
+})
+
+export const NewsLetter = model<TNewsLetter>("NewsLetter", newsLetterSchema);
