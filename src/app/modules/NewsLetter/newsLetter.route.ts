b39600240ebf5d0983c72a1c6f003bdfbd9814b1
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-23 12:04:11 +0600

    feat(contact): add type definition for contact form

--- src/app/modules/NewsLetter/newsLetter.route.ts
+++ src/app/modules/NewsLetter/newsLetter.route.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { NewsLetterControllers } from "./newsLetter.controller";
 import { validateRequestSchema } from "../../middleware/validateRequestSchema";
 import { NewsLetterValidationSchema } from "./newsLetter.validation";
+import { auth } from "../../middleware/auth";
+import { USER_ROLE } from "../User/user.constant";
 
 const router = express.Router();
 
 router.post("/", validateRequestSchema(NewsLetterValidationSchema.createNewsLetterValidationSchema), NewsLetterControllers.createNewsLetterController);
 
-router.get("/", NewsLetterControllers.getAllNewsLettersController)
+router.get("/", auth(USER_ROLE.admin), NewsLetterControllers.getAllNewsLettersController)
 
 export const NewsLetterRoutes = router;
